4f76cf6b5ca115286152d1ea587b210add371405
npretor <npretor@g.clemson.edu>
2022-05-09 22:04:46 -0700

    Motr control class working and reeeeally slow tracking

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
+import time
 from cv2 import HOGDESCRIPTOR_DESCR_FORMAT_ROW_BY_ROW
 import jetson.inference
 import jetson.utils
+import threading
 
 import argparse
 import sys
 import numpy as np
 from simple_pid import PID
-
+from motorControl import MotorController
 import pdb
 
 # parse the command line
@@ -28,30 +29,67 @@ try:
 except:
 	print("")
 	parser.print_help()
-	sys.exit(0)
+	sys.exit(0) 
 
 
+Z_MOTOR_STEP_PIN =  21      # HEADER PIN 21 
+Z_MOTOR_DIR_PIN =   22      # HEADER PIN 22 
+Y_MOTOR_STEP_PIN =  23      # HEADER PIN 23 
+Y_MOTOR_DIR_PIN =   24      # HEADER PIN 24 
+
+# Initialization 
+pidx = PID(1.5, 0.1, 0.05, setpoint=0)
+pidy = PID(1.5, 0.1, 0.05, setpoint=0)
+
+motorX = MotorController(Z_MOTOR_STEP_PIN, Z_MOTOR_DIR_PIN)
+motorX.moveLiveStart() 
 
 
-
-def boundingBoxToError(center, imageSize):
+def boundingBoxToError(bbox_center, imageSize):
     """
     We need to make this a separate function from alignment since we should smooth over any gaps. 
     The models doesn't have any object permanece so we have to make an approximation 
     Input:  
         frames: (tuple) frames to wait after the object dissapears. 
     """
-
+    
     imageCenter = (int(imageSize[0]/2), int(imageSize[1]/2)) 
+    
+    error = (bbox_center[0] - imageCenter[0], bbox_center[1] -  imageCenter[1])
 
     return error 
 
+def processDetections(detections):
+    """
+    ID 1 is a person
+    ID 18 is a cat
+    ID 19 is a dog
+    """
+    bbox_center = (imgx/2, imgy/2)
+    if len(detections) >= 1:
+        for detection in detections:
+            if detection.ClassID == 1:
+                print('person found')
+                return detection.Center
+    return bbox_center
+
+
+            
+
+
+    motorXController.movetime(motorXAxisSpeed, 1000)
+
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
 
 # load the object detection network
 net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
 # create video sources
 input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
 
+#f = open(time.ctime().replace(':','_') + '.txt', 'w')
+
+threshold = 50
+previousError = (1,1)
 
 while True:
     # capture the next image
@@ -61,57 +99,48 @@ while True:
 
     # detect objects in the image (with overlay)
     detections = net.Detect(img, overlay=opt.overlay)
-    # print the detections
     print("detected {:d} objects in image".format(len(detections)))
 
-    #print("Class ID:   ",detection[0].ClassID, "    Type: ",detections[0].Center)
-    #for i in detections:
-    #	print("Classid:  ",detections[i].ClassID)
-        # ID 18 is a cat, ID 19 is a dog
-    if len(detections) >= 1:
-        if detections[0].ClassID == 18:
-            print('Cat found')
-        elif detections[0].ClassID == 19:
-            print('Dog found')
-            #boundingBoxToError(detections[0].Center, imgx, imgy) 
-        else:
-            print('Detections:    ', detections[0].ClassID, 'Center:    ', detections[0].Center)
+    output.Render(img)
+    output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
 
+    # Sort detections for desired object
+    bbox_center = processDetections(detections)
 
+    # Calulate the error relative to the center of the image, leaving a center buffer zone 
+    error = boundingBoxToError(bbox_center, (imgx, imgy))
+
+    if abs(error[0]) > threshold:
+        if error[0] > 0:
+            if previousError[0] > 0:
+                pass
+            else:
+                print('Moving right')
+                motorX.moveLiveSetSpeed(-10000)
+        elif error[0] < 0:
+            if previousError[0] < 0:
+                pass
+            else:
+                print('Moving left')
+                motorX.moveLiveSetSpeed(5000)
+        previousError = error
+    else:
+        print('object on target')
+
+    # PID calculation
+    controlx = pidx(error[0]) 
+    controly = pidy(error[1]) 
+
+    # Motor commands 
+    # direction, speed = process_error(error)
+    # m.move(direction, speed)
+
+    #print("T: {}    cx: {}    cy: {}    ex:   {}  ey:   {}".format(time.ctime(), controlx, controly, error[0], error[1]))
+    #f.write("{}, {}, {} \r\n".format(time.ctime(), (int(controlx), int(controly)), (int(error[0]), int(error[1]))))
+    
 
     print('======================================================================')
-    # render the image
-    #output.Render(img)
-
-    # update the title bar
-    #output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))#
-
-    # print out performance info
-    #net.PrintProfilerTimes()
-
-    # exit on input/output EOS
-    #if not input.IsStreaming() or not output.IsStreaming():
-    #    break
+    if not input.IsStreaming() or not output.IsStreaming():
+        break
 
 
-
-def objectFilter():
-    pass
-
-
-
-
-
-def alignment(xError, yError):
-    """    
-    Goal: Adjust the motor speed as the x and y error converges
-    Inputs:
-        xError: float
-        yError: float
-    """
-
-    return speedX, speedY
-
-
- 
-    
--- motorControl.py
+++ motorControl.py
@@ -1,18 +1,31 @@
+"""
+
+Vertical(camera y axis) motor geartrain
+    Stepper is 1.8 degrees per step
+    Gear 1 is 16 teeth
+    Gear 2 is 160 teeth
+"""
+
 import time
 import Jetson.GPIO as GPIO
+import threading 
+import time
 
-class motorControl:
+
+class MotorController:
     def __init__(self, MOTOR_STEP_PIN, MOTOR_DIR_PIN, step_angle_deg=1.8, micro_steps=.125):
+        self.motorSpeed = 0
         self.step_pin = MOTOR_STEP_PIN 
         self.dir_pin = MOTOR_DIR_PIN 
+        self.state = 'idle'
         GPIO.setwarnings(True)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(MOTOR_STEP_PIN, GPIO.OUT, initial=GPIO.LOW) 
         GPIO.setup(MOTOR_DIR_PIN, GPIO.OUT, initial=GPIO.LOW) 
         self.step_angle_deg = step_angle_deg
-        self.micro_steps = micro_steps
-    
-    def move(self, move_angle, speed, angle='degrees'):
+        self.micro_steps = micro_steps 
+
+    def moveAngle(self, move_angle, speed, angle='degrees'):
         """
         Speed is in degrees per second
         """
@@ -25,5 +38,123 @@ class motorControl:
             GPIO.output(self.dir_pin, GPIO.LOW)
             time.sleep(delay)
 
+    def moveTime(self, moveduration, direction, speed):
+        """
+        Time: (seconds)
+        Speed: (steps/second) 
+        Direction: left, right
+        """
+        if direction == 'left':
+            GPIO.output(self.dir_pin, GPIO.LOW)
+        elif direction == 'right':
+            GPIO.output(self.dir_pin, GPIO.HIGH)
+        else:
+            print('direction invalid')
+            exit(0)
+
+        endtime = moveduration + time.time()
+        print(endtime,time.time(),endtime-time.time())  
+        if speed == 0:
+            time.sleep(moveduration)
+            return 0
+        else:
+            delay = 1 / (speed*2)
+        print('delay',delay)
+        while time.time() <= endtime:
+            GPIO.output(self.step_pin, GPIO.HIGH)
+            time.sleep(delay)
+            GPIO.output(self.step_pin, GPIO.LOW)
+            time.sleep(delay)
+        return 0
+
+    def _moveLive(self):
+        """
+        Primary function that handles the motion pulses
+        Speed: steps / second
+        """
+
+        while self.state == 'running':
+            if self.motorSpeed == 0:
+                # sleep instead of moving for a constant time
+                time.sleep(0.001) 
+            else:
+                # Direction
+                if self.motorSpeed > 0:
+                    GPIO.output(self.dir_pin, GPIO.LOW)
+                elif self.motorSpeed < 0:
+                    GPIO.output(self.dir_pin, GPIO.HIGH)
+                                
+                delay = ((1/(abs(self.motorSpeed))) / 2 )
+                print('delay:   ', delay)
+                GPIO.output(self.step_pin, GPIO.HIGH)
+                time.sleep(delay) 
+                GPIO.output(self.step_pin, GPIO.LOW)
+                time.sleep(delay) 
+
+    def moveLiveStart(self):
+        """
+        Starts the thread
+        """
+        self.state = 'running'
+        self.t = threading.Thread(target=self._moveLive)
+        self.t.start()
+
+    def moveLiveSetSpeed(self, speed):
+        self.motorSpeed = speed
+
+    def moveLiveEnd(self):
+        self.state = 'idle'
+        self.t.join()
+
     def close(self):
         GPIO.cleanup([self.step_pin, self.dir_pin]) 
+
+
+def simpleTest():
+    m = MotorController(21,22)
+
+    locations = [(-100, 10),(100, -10) , (-5, -5)]
+    threshold = 10
+
+    for x,y in locations:
+        print("processing: ",x,y)
+        # Is the value within +_threshold range
+        if abs(x) <= threshold:
+            time.sleep(.5)
+
+        else:
+            speed = 1000
+            # If it's outside, determine the direction to move
+            if x > 0: 
+                direction = 'right'
+            else: 
+                direction = 'left'
+        
+            m.moveTime(1, direction, speed)
+            time.sleep(1)
+    m.close()
+
+#simpleTest() 
+
+
+def test():
+    m = MotorController(21, 22)
+    m.moveLiveStart()
+
+    # Counterclockwise
+    m.moveLiveSetSpeed(2000)
+
+    print('sleeping')
+    time.sleep(1)
+
+    print('awake, changing speed')
+    # Clockwise
+    m.moveLiveSetSpeed(-2000)
+
+    time.sleep(1)
+
+    print('closing')
+    m.moveLiveEnd()
+    m.close()
+
+#test()
